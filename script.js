a2cff65b1466521313fed81983d9d9ac4a855c2c
Abhishek Roushan <abhishekashu23@gmail.com>
2023-09-06 12:16:57 +0530

    V1.1

---
+++ script.js
@@ -0,0 +1,20 @@
+let scrollContainer = document.querySelector(".gallery");
+let backBtn = document.getElementById("backbtn");
+let nextBtn = document.getElementById("nextbtn");
+
+
+scrollContainer.addEventListener("wheel", (e)=>{
+    e.preventDefault();
+    scrollContainer.scrollLeft +=e.deltaY;
+    // scrollContainer.style.scrollBehavior = "auto";
+});
+
+nextBtn.addEventListener("click", ()=>{
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft += 300;
+});
+
+backBtn.addEventListener("click", ()=>{
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft -= 300;
+});
